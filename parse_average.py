a74e65a8c1e4086de89bcbff9f064a0f6d01101c
Victor Gomes <victorncg@gmail.com>
2016-04-23 21:21:02 -0400

    parse_average.py

---
+++ parse_average.py
@@ -0,0 +1,27 @@
+# Enter the file name
+filename = raw_input("Enter file name: ")
+file = open(filename)
+
+# Calculate the sum of the numbers and count the number of rows
+total = 0
+i = 0
+
+for line in file:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    
+    # Extract number from the string
+    number = line[20::1]
+    # Convert it to float format
+    number = float (number)
+    
+    # Calculate the sum
+    total = total + number
+    
+    # Count the number of rows
+    i = i + 1
+    
+# Calculate the average
+average = total/i
+
+print "Average spam confidence:", average
+ 
